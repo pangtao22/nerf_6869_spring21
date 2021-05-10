26bd55924b90736675188a6894ba06ebae8a23eb
Unknown <pangtao@mit.edu>
2021-05-10 10:57:24 -0400

    Datasets in separate files.

---
+++ my_tiny_nerf_dataloader.py
@@ -0,0 +1,36 @@
+import os
+import PIL
+
+import numpy as np
+import torch
+from torch.utils.data import Dataset
+
+
+class MyTinyNerfDataset(Dataset):
+    def __init__(self, transform):
+        data = np.load('tiny_nerf_data.npz')
+        images = data['images']
+        poses = data['poses']
+        self.focal = data['focal']
+        # H, W = images.shape[1:3]
+        self.transform = transform
+
+        self.image_test = torch.tensor(
+            images[101], dtype=torch.float32).permute(2, 0, 1)
+        self.pose_test = torch.tensor(poses[101], dtype=torch.float32)
+
+        self.images_train = []
+        for img_np in images[:100]:
+            self.images_train.append(
+                PIL.Image.fromarray((img_np * 255).astype(np.uint8)))
+
+        self.poses_train = torch.tensor(poses[:100], dtype=torch.float32)
+
+    def get_focal(self):
+        return self.focal.item()
+
+    def __len__(self):
+        return len(self.images_train)
+
+    def __getitem__(self, idx):
+        return self.transform(self.images_train[idx]), self.poses_train[idx]
--- tiny_nerf_torch.py
+++ tiny_nerf_torch.py
@@ -1,17 +1,15 @@
-import os
-import PIL
 import copy
 
 import torch
 import torchvision
 import torch.nn.functional as F
-from torch.utils.data import Dataset
 from torchvision import transforms
 from torchvision.io import read_image
-import json
 # from pydrake.math import RigidTransform, RollPitchYaw
 import numpy as np
 import matplotlib.pyplot as plt
+
+from my_tiny_nerf_dataloader import MyTinyNerfDataset
 
 # Detect if we have a GPU available
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
@@ -20,64 +18,22 @@ if torch.cuda.is_available():
 else:
     print("WARNING: Could not find GPU! Using CPU only")
 
-#%% dataloaders
-# images
-H = 10
-W = 256
+
+#%% TinyNerfDataloader
+H = 100
+W = 100
 data_transform = transforms.Compose([
-        transforms.CenterCrop((H, 320)),
         transforms.Resize((H, W)),
         transforms.ToTensor(),
 ])
 
+my_tiny_nerf_dataset = MyTinyNerfDataset(data_transform)
+focal = my_tiny_nerf_dataset.get_focal()
 
-class MyNerfDataset(Dataset):
-    def __init__(self, data_subfolder_name: str, data_transform):
-        self.transform = data_transform
-        data_folder_path = os.path.join(os.getcwd(), "data")
-        pose_file_path = os.path.join(
-            data_folder_path, data_subfolder_name + '.json')
-        self.img_folder_path = os.path.join(data_folder_path,
-                                            data_subfolder_name)
-        with open(pose_file_path, "r") as f:
-            self.intrinsics_and_pose = json.load(f)
+dataloader = torch.utils.data.DataLoader(
+    my_tiny_nerf_dataset, batch_size=1, shuffle=True)
 
-        img_file_names = []
-        for name in os.listdir(self.img_folder_path):
-            if name.endswith('png'):
-                img_file_names.append(name)
-        img_file_names.sort()
-
-        # load images here.
-        self.images = []
-        for img_file_name in img_file_names:
-            img_path = os.path.join(self.img_folder_path, img_file_name)
-            self.images.append(PIL.Image.open(img_path))
-
-    def get_W(self):
-        return self.intrinsics_and_pose["w"]
-
-    def get_H(self):
-        return self.intrinsics_and_pose["h"]
-
-    def get_focal(self):
-        K = self.intrinsics_and_pose["camera_intrinsics"]
-        return K[0][0]
-
-    def __len__(self):
-        return len(self.images)
-
-    def __getitem__(self, idx):
-        image = self.transform(self.images[idx])
-        X_WC = torch.tensor(self.intrinsics_and_pose['frames'][0]['X_WC'])
-        return image, X_WC
-
-
-my_nerf_dataset = MyNerfDataset('train', data_transform)
-
-dataloader = torch.utils.data.DataLoader(my_nerf_dataset, batch_size=1,
-                                         shuffle=True)
-
+X_WC_validation = my_tiny_nerf_dataset.pose_test
 
 #%% look at one data point.
 torch.manual_seed(2021)
@@ -118,8 +74,8 @@ def get_rays(H, W, focal, X_WC):
     # focal = 1
     # X_WC = torch.eye(4)
     jj, ii = torch.meshgrid(
-        torch.arange(H, dtype=torch.float32, device=device),
-        torch.arange(W, dtype=torch.float32, device=device))
+        torch.arange(H, dtype=torch.float32),
+        torch.arange(W, dtype=torch.float32))
     directions = torch.stack(
         [(ii - W * .5) / focal, -(jj - H * .5) / focal, -torch.ones_like(ii)],
         dim=2)
@@ -131,25 +87,23 @@ def get_rays(H, W, focal, X_WC):
 
 def run_network_on_points(model, pts, batch_size: int):
     n_pts = pts.shape[0]
-    return torch.cat(
-        [model(pts[i: i + batch_size]) for i in range(0, n_pts, batch_size)])
+    return torch.cat([model(pts[i: i + batch_size])
+                      for i in range(0, n_pts, batch_size)])
 
 
-def render_rays(model, rays_o, rays_d, near, far, n_samples, rand=False):
-
+def render_rays(model, rays_o, rays_d, near, far,
+                n_samples_per_ray, rand=False):
     H = rays_d.shape[0]
     W = rays_d.shape[1]
 
     # Compute 3D query points
-    z_values = torch.linspace(near, far, n_samples, device=device)
+    z_values = torch.linspace(near, far, n_samples_per_ray)
     if rand:
-        d_grid = (far - near) / n_samples
+        d_grid = (far - near) / n_samples_per_ray
         z_values = (z_values +
-                    torch.rand(list(rays_o.shape[:2]) + [n_samples],
-                               device=device) * d_grid)
+                    torch.rand(list(rays_o.shape[:2]) + [n_samples_per_ray]) * d_grid)
     else:
-        z_values = torch.broadcast_to(z_values, [H, W, n_samples])
-
+        z_values = torch.broadcast_to(z_values, [H, W, n_samples_per_ray])
     # rays_d[..., None, :] shape: (H, W, 1, 3)
     # z_vals shape: (n_samples) or (H, W, n_samples)
     # z_values[..., :, None] shape: (n_samples, 1) or (H, W, n_samples, 1)
@@ -159,7 +113,9 @@ def render_rays(model, rays_o, rays_d, near, far, n_samples, rand=False):
     # Run network.
     # pts_flat shape: (H * W * n_samples, 3)
     pts_flat = encode_position(torch.reshape(pts, [-1, 3]), l_embed=6)
-    raw = run_network_on_points(model, pts_flat, 4096)
+    pts_flat = pts_flat.to(device)
+    raw = run_network_on_points(model, pts_flat,
+                                batch_size=min(pts_flat.shape[0], 32 * 1024))
     raw = raw.reshape(list(pts.shape[:-1]) + [4])
     # raw shape now: (H, W, n_samples, 4)
 
@@ -167,14 +123,16 @@ def render_rays(model, rays_o, rays_d, near, far, n_samples, rand=False):
     # sigma_a.shape: (H, W, n_samples)
     # rgb.shape: (H, W , n_samples, 3)
     sigma_a = F.relu(raw[..., 3])
-    rgb = F.sigmoid(raw[..., :3])
+    rgb = torch.sigmoid(raw[..., :3])
 
     # Do volume rendering.
-    detla_z = torch.cat([z_values[..., 1:] - z_values[..., :-1],
-                         torch.broadcast_to(torch.tensor(1e10, device=device),
-                                            [H, W, 1])], dim=-1)
-    alpha = 1. - torch.exp(-sigma_a * detla_z)
-    t = torch.ones_like(alpha, device=device)
+    z_values = z_values.to(device)
+    delta_z = torch.cat([
+        z_values[..., 1:] - z_values[..., :-1],
+        torch.broadcast_to(torch.tensor(1e10, device=device), [H, W, 1])],
+        dim=-1)
+    alpha = 1. - torch.exp(-sigma_a * delta_z)
+    t = torch.ones_like(alpha)
     t[..., 1:] = (1. - alpha + 1e-10)[..., :-1]
     t = torch.cumprod(t, dim=-1)
     weights = alpha * t
@@ -210,37 +168,19 @@ class Nerf(torch.nn.Module):
 
 
 #%% train network
-D_network = 3
-W_network = 64
+D_network = 4
+W_network = 128
 l_embed = 6
-
-# H_img = my_nerf_dataset.get_H()
-# W_img = my_nerf_dataset.get_W()
-focal_length = my_nerf_dataset.get_focal()
 
 num_epochs = 10
 
-model = Nerf(D=D_network, W=W_network, l_embed=l_embed).to(device)
+model = Nerf(D=D_network, W=W_network, l_embed=l_embed)
+best_model_weights = copy.deepcopy(model.state_dict())
+model.to(device)
 optimizer = torch.optim.Adam(model.parameters(), lr=5e-4)
 mse_loss = torch.nn.MSELoss()
 
-
-# test pose
-r = 2.8
-# X_WC_validation = RigidTransform()
-# X_WC_validation.set_translation([0, r, 0])
-# X_WC_validation.set_rotation(
-#     RollPitchYaw(-np.pi/2, 0, np.pi).ToRotationMatrix())
-# X_WC_validation = torch.tensor(X_WC_validation.GetAsMatrix4(),
-#                                dtype=torch.float32)
-X_WC_validation = torch.tensor(
-    [[-1, 0, 0, 0],
-     [0, 0, -1, r],
-     [0, -1, 0, 0],
-     [0, 0, 0, 1]], dtype=torch.float32).to(device)
-
 # keeping track of models.
-best_model_weights = copy.deepcopy(model.state_dict())
 loss_history = []
 best_loss = np.inf
 
@@ -253,18 +193,18 @@ for epoch in range(num_epochs):
     running_loss = 0.
     for i, (images, X_WCs) in enumerate(dataloader):
         # print(i)
-        image = images[0].to(device).permute([1, 2, 0])
-        X_WC = X_WCs[0].to(device)
+        X_WC = X_WCs[0]
 
         optimizer.zero_grad()
 
         # forward
-        rays_o, rays_d = get_rays(H=H, W=W, focal=focal_length,
-                                  X_WC=X_WC.to(device))
+        rays_o, rays_d = get_rays(H=H, W=W, focal=focal, X_WC=X_WC)
         with torch.set_grad_enabled(True):
             rgb, depth = render_rays(
                 model=model, rays_o=rays_o, rays_d=rays_d,
-                near=0.5, far=6.0, n_samples=64, rand=True)
+                near=0.5, far=6.0, n_samples_per_ray=64, rand=True)
+
+            image = images[0].permute([1, 2, 0]).to(device)
             loss = mse_loss(rgb, image)
 
             loss.backward()
@@ -273,19 +213,20 @@ for epoch in range(num_epochs):
         running_loss += loss.item()
 
     # render an image and print loss.
-    epoch_loss = running_loss / len(my_nerf_dataset)
+    epoch_loss = running_loss / len(dataloader.dataset)
     if epoch_loss < best_loss:
         best_loss = epoch_loss
-        best_model_weights = copy.deepcopy(model.state_dict())
+        best_model_weights = copy.deepcopy(model.to('cpu').state_dict())
+        model.to(device)
 
     loss_history.append(epoch_loss)
 
     #%%
     model.eval()
-    rays_o, rays_d = get_rays(H, W, focal_length, X_WC_validation.to(device))
+    rays_o, rays_d = get_rays(H, W, focal, X_WC_validation)
     rgb, depth = render_rays(
         model=model, rays_o=rays_o, rays_d=rays_d,
-        near=0.5, far=6.0, n_samples=64, rand=False)
+        near=0.5, far=6.0, n_samples_per_ray=64, rand=False)
 
     plt.figure(dpi=200)
     plt.subplot(121)
@@ -295,13 +236,4 @@ for epoch in range(num_epochs):
     plt.plot(loss_history)
     plt.show()
 
-
-
-
-#%%
-
-plt.figure(dpi=200)
-plt.imshow(rgb.detach().numpy())
-plt.axis('off')
-plt.show()
 
